934a78f67ae27215dabeb19679df8f6b279a266a
4bhay <abhay.goel0674@gmail.com>
2025-03-15 23:16:56 +0530

    Changing Props-Page

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useMutation, useQuery } from "convex/react";
@@ -10,65 +9,55 @@ import Cover from "@/app/(main)/_components/Cover";
 import { Skeleton } from "@/components/ui/skeleton";
 import Editor from "@/components/Editor";
 
-interface props {
-    params: {
-        documentId: Id<"documents">
-    }
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">;
+  };
 }
-const DocumentIdPage = ({ params }: props) => {
 
-    const update = useMutation(api.documents.update);
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const update = useMutation(api.documents.update);
 
-    const document = useQuery(api.documents.getById, {
-        documentId: params.documentId
-    });
+  // Fetch document data
+  const document = useQuery(api.documents.getById, { documentId: params.documentId });
 
-    if (document === undefined) {
-        return (
-            <div>
-                 <Cover.skeleton />
-                 <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10 ">
-                    <div className="sapce-y-4 pl-8 pt-4">
-                        <Skeleton className="h-14 w-[50%]" />
-                        <Skeleton className="h-14 w-[80%]" />
-                        <Skeleton className="h-14 w-[40%]" />
-                        <Skeleton className="h-14 w-[60%]" />
-                    </div>
-                 </div>
-            </div>
-           
-        )
-
-    }
-
-    if (document === null){
-        return <div>Not Found</div>
-    }
-
-
-    const onChange = (val :  string)=> {
-        update({
-            id : params.documentId  ,
-            content : val
-        })
-    }
+  // Handle loading state
+  if (document === undefined) {
     return (
-        <div className="pb-40">
-            <Cover
-            url = {document.coverImage }
-            />
-            <div className="md:max-w-3xl lg:max-w-3xl mx-auto">
-                <Toolbar 
-                initialData={document}
-                />
-                <Editor
-                    onChange = {onChange}
-                    initialContent = {document.content}
-                />
-            </div>
+      <div>
+        <Cover.skeleton /> 
+        <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
+          <div className="space-y-4 pl-8 pt-4">
+            <Skeleton className="h-14 w-[50%]" />
+            <Skeleton className="h-14 w-[80%]" />
+            <Skeleton className="h-14 w-[40%]" />
+            <Skeleton className="h-14 w-[60%]" />
+          </div>
         </div>
+      </div>
+    );
+  }
 
-    )
+  if (document === null) {
+    return <div className="text-center text-red-500 font-bold">Document Not Found</div>;
+  }
+
+  const onChange = (val: string) => {
+    update({
+      id: params.documentId,
+      content: val,
+    });
+  };
+
+  return (
+    <div className="pb-40">
+      <Cover url={document.coverImage} />
+      <div className="md:max-w-3xl lg:max-w-3xl mx-auto">
+        <Toolbar initialData={document} />
+        <Editor onChange={onChange} initialContent={document.content} />
+      </div>
+    </div>
+  );
 };
 
 export default DocumentIdPage;
